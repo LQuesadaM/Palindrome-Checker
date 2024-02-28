ace54188ce5b5da911fff43bdcc885078ebb7820
LQuesadaM <luisquesada163@gmail.com>
2024-02-28 09:34:14 -0300

    if statement for logic of compare

--- script.js
+++ script.js
@@ -4,11 +4,21 @@ const result = document.querySelector("#result");
 
 const checkPalindrome = () => {
     const regex = /[^a-z0-9]/g;
+    const textInputLowerCase = textInput.value.toLowerCase()
     const textFilter = textInput.value
         .toLowerCase()
         .replace(regex, "")
         .split("")
+        .reverse()
         .join("")
+
+    if (textFilter === "") {
+        alert("Please input a value")
+    } else if (textFilter === textInputLowerCase) {
+        result.textContent = `${textInput.value} is a palindrome.`
+    } else {
+        result.textContent = `${textInput.value} is not a palindrome.`
+    }
 }
 
 checkButton.addEventListener("click", checkPalindrome);
